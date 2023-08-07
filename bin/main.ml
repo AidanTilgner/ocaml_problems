ea7be25e9cb9635a6cb8d7dc35dc163ff7f3fead
atilgner <aidantilgner02@gmail.com>
2023-08-07 09:02:48 -0700

    feat: added 'at' function

--- bin/main.ml
+++ bin/main.ml
@@ -3,4 +3,5 @@ open Ocaml_problems
 let () =
   print_endline "\n";
   Beginner.last [ "a"; "b"; "c"; "d" ] |> Utils.print_none_safe;
-  Beginner.last_two [ "a"; "b"; "c"; "d"; "e" ] |> Utils.print_none_safe_tuple
+  Beginner.last_two [ "a"; "b"; "c"; "d"; "e" ] |> Utils.print_none_safe_tuple;
+  Beginner.at 3 [ "a"; "b"; "c"; "d"; "e" ] |> Utils.print_none_safe
--- lib/beginner.ml
+++ lib/beginner.ml
@@ -1,9 +1,14 @@
 (** [last l] returns the last element of [l] if [l] is not empty. *)
 let rec last = function [] -> None | [ x ] -> Some x | _ :: t -> last t
 
-(** [last_two l] return the last two elements of [l] if [l] is not empty and contains more than one element *)
+(** [last_two l] return the last two elements of [l] if [l] is not empty and contains more than one element. *)
 let rec last_two = function
   | [] -> None
   | [ _ ] -> None
   | [ x; y ] -> Some (x, y)
   | _ :: t -> last_two t
+
+(** [at l] return the element in [l] at index [l]. *)
+let rec at k = function
+  | [] -> None
+  | h :: t -> if k = 0 then Some h else at (k - 1) t
